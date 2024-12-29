225a3866c246ca29ee8de90cbf8925bb8bc89c86
Vovan1968 <vladimir.ryabov68@gmail.com>
2024-12-29 19:26:42 +0300

    final
    last changes

---
+++ My_project/main/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("<h1>My first project on Django</h1>")
+def data(request):
+    return HttpResponse("<h1>New page with data</h1>")
+#def test(request):
+   # return HttpResponse("<h1>New page for test</h1>")
+
+
+
+def test(request):
+    return render(request, 'test.html')
